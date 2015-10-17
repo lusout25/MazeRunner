5360d0cbf559c5e0266d331fc1384a25bb56d20b
lukejsouthard <lusout25@gmail.com>
2015-10-17 14:13:45 -0500

    add untracked files

---
+++ GameEngine3D/Shared.h
@@ -0,0 +1,17 @@
+/***********************************************************
+	Constants used in Game Engine
+***********************************************************/
+
+namespace GameEngine3D {
+	
+#ifndef SHARED_H
+#define SHARED_H
+	
+	const int NUM_VERTICES_WALL = 36;
+	const int NUM_3D_VERTEX = 3;
+	const int NUM_RGBA_COLOR = 4;
+	const float WALL_EDGE_LENGTH = 1.0f;
+
+#endif
+
+}
